564be067e62dc9b8646dd6c153e6c0034491c2fd
Olenka Sanchez <olesa600@gmail.com>
2022-10-22 13:35:35 -0500

    ADD: Unique index to username

--- db/schema.rb
+++ db/schema.rb
@@ -113,10 +113,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_163441) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "first_name"
+    t.string "last_name"
     t.string "role"
     t.integer "critics_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
