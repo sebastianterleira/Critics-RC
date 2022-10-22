054ca9765ff3d95484da6f7fdf4480ce7bac4df8
Elías Mesones <eliasmesones@gmail.com>
2022-10-22 00:19:31 -0500

    ADD: Routes and show modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,34 @@
 Rails.application.routes.draw do
-  resources :genres
-  resources :platforms
-  resources :involved_companies
-  resources :critics
-  resources :games
-  resources :companies
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "games#index"
+
+  resources :critics
+  resources :involved_companies
+  resources :games do
+    resources :critics
+
+    # /games/:id/add_genre
+    post "add_genre", on: :member
+    # /games/:id/remove_genre
+    delete "remove_genre", on: :member
+
+    # /games/:id/add_platform
+    post "add_platform", on: :member
+    # /games/:id/remove_platform
+    delete "remove_platform", on: :member
+  end
+  resources :platforms
+  resources :genres
+  resources :companies do
+    resources :critics
+  end
+  resources :users, only: :new
+
+  get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+  delete "/sessions", to: "sessions#destroy"
+
+  get "/profile", to: "users#edit"
+  patch "/profile", to: "users#update"
+  post "/profile", to: "users#create"
+  
 end
