3bcaab3a10e9c6356c8c169376d3091631efec80
Olenka Sanchez <olesa600@gmail.com>
2022-10-21 23:53:08 -0500

    ADD: User id reference to critics

--- db/migrate/20221021024016_create_critics.rb
+++ db/migrate/20221021024016_create_critics.rb
@@ -3,6 +3,7 @@ class CreateCritics < ActiveRecord::Migration[7.0]
     create_table :critics do |t|
       t.string :title
       t.text :body
+      t.references :user, null: false, foreign_key: true
       t.references :criticable, polymorphic: true, null: false
 
       t.timestamps
