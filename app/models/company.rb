0c9603667b8796ed7d595ddb68efd9c2010b1bdf
Olenka Sanchez <olesa600@gmail.com>
2022-10-21 23:49:34 -0500

    change alias for critics in Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,5 +4,8 @@ class Company < ApplicationRecord
     # Associations
     has_many :involved_companies, dependent: :destroy
     has_many :games, through: :involved_companies, counter_cache: true
-    has_many :critics, as: :criticable, dependent: :destroy
+    has_many :received_critics, class_name: "Critic", 
+    as: :criticable, 
+    dependent: :destroy
+
 end
